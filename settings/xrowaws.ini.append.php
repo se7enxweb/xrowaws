7e8accec392243f527bb053f864cc63b8c0d0ce4
xinyuexrow <wang@xrow.de>
2013-09-03 14:02:09 +0200

    Attributes in Memcache

--- settings/xrowaws.ini.append.php
+++ settings/xrowaws.ini.append.php
@@ -1,8 +1,11 @@
 <?php /* #?ini charset="utf-8"?
 
-#[Settings]
-#AWSKey=
-#SecretKey=
-#Bucket=
+[Settings]
+AWSKey=
+SecretKey=
+Bucket=storage-bucket
 
+[MemcacheSettings]
+Host=192.168.0.1
+Port=11211
 */ ?>
